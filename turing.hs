869848a32de1c9e628302281d487bafae8e9b428
Miguel A. Santos <msantos@sdf.org>
2016-06-14 12:53:32 -0400

    Added basic explanation; cleaned output

--- turing.hs
+++ turing.hs
@@ -1,5 +1,40 @@
--- A dirty implementation of a Turing Machine in Haskell.
+{-- A dirty implementation of a Turing Machine in Haskell.
+  
+   Basically, it consists on a 'set' of Symbols, States & Transitions. 
+   Each States is given as a pair of type (Statelabel, [Transition]) listing all possible transitions off the given states.
+   There is an infinite tape where the input 'code' is given and where the machine reads off from and writes to.
+   The rest is boilerplate.
+ 
+   Main abstract data types (ADT) are:
+          data Symbol =  Blank | Zero | One  deriving Eq
+          type Tape = [Symbol]
+          data Statelabel = Q0 | Q1 | Q2 | HLT deriving (Eq,Show)
+          data Transition = Nil | Trans Symbol Symbol HeadMove Statelabel 
+          type State = (Statelabel, [Transition])
+          type TransitionTable = [State]
+          --Basic definition of a Turing Machine (TM)
+          data TM = TM {
+                        --current state of the machine  out of a finite set  of states 'Statelabel'
+                        tmQ::Statelabel  
+                        ,
+                        --initial state
+                        tmQo::Statelabel
+                        ,
+                        --distinguished blank symbol
+                        tmSo::Symbol
+                        ,
+                        --transition table := partial function tmQ x tm∑ -> { tm∑ } x {HeadMove} x { tmQ }
+                        tmD::TransitionTable
+                        ,
+                        --current position along the tape --this is not nice: as TM has to know about a 'tape' definied outside 
+                        tmpos:: Tposition
+                        ,
+                        --the TM's tape
+                        tmtape::Tape
+                       }
+
 -- TODO: check consistencym, ...
+--}
 -- TypeSynonymInstances: use type synonyms in instance definitions
 -- FlexibleInstances: using complex, nested, or multiparameter types in instance definitions
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances , OverlappingInstances #-}
@@ -151,7 +186,7 @@ instance TuringMachine TM where
                | m == S = cp
              --}
      graph tm = do
-                print $ (tmD tm)
+                putStrLn $ "Transition table:\n"++ show (tmD tm)
 
 printCurrentMachineState :: TM -> IO ()
 printCurrentMachineState m = do 
@@ -162,7 +197,7 @@ printCurrentMachineState m = do
 --Run a turing machine
 runTM :: TM -> Tape -> IO (TM)
 runTM tm tp = do
-         putStrLn $ "Starting...\nTape: " ++  (fromTapeToString' tp)
+         putStrLn $ "Tape: " ++  (fromTapeToString' tp) ++ "\nStarting..."
          let thistm = tm{tmtape=tp}
          let loop m 
                     | (cstate m) /= HLT = do
